d5e68da4c56965f0867f3ba1eecfd33fb1735d43
Yasin Hançer <yasinhancerpc@hotmail.com>
2021-02-13 22:47:45 +0300

    13 Şubat 1.KAYIT

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,38 @@
+﻿using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")] //route: bu istek yapılırken, nasıl ulaşsın?
+    [ApiController] //ATTRIBUTE - class ile ilgili bilgi verme, imzalama yöntemi ( bu class bir controller'dır anlamında )
+    public class ProductsController : ControllerBase
+    {
+        //IoC Container -> Inversion of Control / değişimin kontrolü / bellekte newleme olayı / IoC arka planda benim yerime newler 
+        
+        
+        //DEPENDENCY INJECTION -> BAĞIMLILIK ENJEKSİYONU
+        IProductService _productService;
+        public ProductsController(IProductService productService) //Controller sen bir IProductService bağımlısısın 
+        {
+            _productService = productService;
+        }
+        
+        [HttpGet]
+        public List<Product> Get()
+        {
+            //DEPENDENCY CHAIN - BAĞIMLILIK ZİNCİRİ 
+            
+            var result = _productService.GetAll();
+            return result.Data;
+
+        }
+    }
+}
